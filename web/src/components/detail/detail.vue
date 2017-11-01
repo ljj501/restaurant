b74a8ce5e1a1b47dde6e6edeee39678adc3acda3
ljj <1103339447@qq.com>
2017-11-01 16:04:41 +0800

    移动端合并

--- web/src/components/detail/detail.vue
+++ web/src/components/detail/detail.vue
@@ -1,17 +1,6 @@
 <template>
-    <div class="content"  v-if="this.status = 'false'">
-        <!-- <div class="navleft"> -->
-            <!-- <ul>
-                <li>热销榜</li>
-                <li>米线</li> 
-                <li>麻辣烫</li>
-                <li>生煎</li>
-                <li>粥</li>
-                <li>大龙虾</li>
-                <li>大闸蟹</li>
-            </ul> -->
+    <div class="content">
             <navleft  api=""></navleft>
-      
             <foodslist></foodslist>
     </div>
 
@@ -20,6 +9,7 @@
     import './detail.scss'
     import foodslist from '../content/foodslist.vue'
     import navleft from '../content/navleft.vue'
+    
     export default{
         data:function(){
             return {
