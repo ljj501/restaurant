98fcdf874a8b714cec91c5739d19c716efd9a13e
ljj <1103339447@qq.com>
2017-11-02 11:00:30 +0800

    重组

--- web/src/components/goodsform/goodsform.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div>
-		<h1>goods form</h1>
-	</div>
-</template>
-
-<script type="text/javascript">
-	export default {}
-</script>
--- web/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/'
-import $ from 'jquery'
-new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
-})
